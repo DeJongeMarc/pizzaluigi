79cb5436e445512178ce4e2ee0a7e02c9cf08e91
VDABHEVERLEE\marc.de.jonge <marc.de.jonge1@telenet.be>
2017-09-20 09:34:45 +0200

    index.html verwijderd en fout in InexServlet verbeterd

--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -1,6 +1,9 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet("/index.html")
+@WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -19,7 +22,14 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.println("<!doctype html>");
+		out.println("<html lang='nl'><head>");
+		out.println("<title>Pizza Luigi</title></head>");
+		out.println("<body><h1>");
+		int uur = LocalDateTime.now().getHour();
+		out.print(uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
+		out.println("</h1></body></html>");
 	}
 
 }

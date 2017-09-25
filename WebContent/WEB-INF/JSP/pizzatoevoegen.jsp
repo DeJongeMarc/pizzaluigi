8112d787b2a6c45a340f22dfc202dcae064ae986
Marc De Jonge <marc.de.jonge1@telenet.be>
2017-09-25 07:34:13 +0200

    Aanpassingen hoofdstuk 19 (tot 19.4)

---
+++ WebContent/WEB-INF/JSP/pizzatoevoegen.jsp
@@ -0,0 +1,27 @@
+<%@page contentType='text/html' pageEncoding='UTF-8' session='false'%>
+<%@taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core'%>
+<!doctype html>
+<html lang='nl'>
+<head>
+<c:import url='/WEB-INF/JSP/head.jsp'>
+	<c:param name='title' value='Pizza toevoegen' />
+</c:import>
+</head>
+<body>
+	<c:import url='/WEB-INF/JSP/menu.jsp' />
+	<h1>Pizza toevoegen</h1>
+	<form method='post' id='toevoegform'>
+		<label>Naam<span>${fouten.naam}</span> <input name='naam'
+			value='${param.naam}' autofocus required></label> <label>Prijs<span>${fouten.prijs}</span>
+			<input name='prijs' value='${param.prijs}' type='number' min='0'
+			required step='0.01'>
+		</label>
+		<div>
+			<label> <input type='checkbox' name='pikant' value='pikant'>
+				Pikant
+			</label>
+		</div>
+		<input type='submit' value='Toevoegen' id='toevoegknop'>
+	</form>
+</body>
+</html>
---
+++ src/be/vdab/servlets/PizzaToevoegenServlet.java
@@ -0,0 +1,62 @@
+package be.vdab.servlets;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import be.vdab.entities.Pizza;
+import be.vdab.repositories.PizzaRepository;
+import be.vdab.util.StringUtils;
+
+/**
+ * Servlet implementation class PizzaToevoegenServlet
+ */
+@WebServlet("/pizzas/toevoegen.htm")
+public class PizzaToevoegenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/pizzatoevoegen.jsp";
+	private static final String REDIRECT_URL = "/pizzas.htm";
+	private final PizzaRepository pizzaRepository = new PizzaRepository();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher(VIEW).forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		Map<String, String> fouten = new HashMap<>();
+		String naam = request.getParameter("naam");
+		if (! Pizza.isNaamValid(naam)) {
+			fouten.put("naam", "verplicht");
+		}
+		String prijsString = request.getParameter("prijs");
+		BigDecimal prijs = null;
+		if (StringUtils.isBigDecimal(prijsString)) {
+			prijs = new BigDecimal(prijsString);
+			if (!Pizza.isPrijsValid(prijs)) {
+				fouten.put("prijs", "tik een positief getal");
+			}
+		} else {
+			fouten.put("prijs", "tik een getal");
+		}
+		if (fouten.isEmpty()) {
+			boolean pikant = "pikant".equals(request.getParameter("pikant"));
+			pizzaRepository.create(new Pizza(naam, prijs, pikant));
+			response.sendRedirect(request.getContextPath() + REDIRECT_URL);
+		} else {
+			request.setAttribute("fouten", fouten);
+			request.getRequestDispatcher(VIEW).forward(request, response);
+		}
+	}
+}
